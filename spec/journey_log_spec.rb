0fac140080bdc59a879c988d188bf05ea9521086
Jakephillips55 <Jakep629@gmail.com>
2019-12-18 15:43:45 +0000

    15 partial

--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,12 +1,13 @@
 require 'journey_log'
 
 describe JourneyLog do
-
+let(:station1) { double :station}
+let(:station2) { double :station}
   it 'should have the journey class paramenter when we initialize a journey log' do
     expect { subject.journey_class }.to_not raise_error
   end
 
     it 'should start a new journey with an entry station when calling the start method' do
-      
+
     end
 end
